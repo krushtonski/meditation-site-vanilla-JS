54741f664197c9da9034f0c99e5bf604c721a359
krushtonski <36168072+krushtonski@users.noreply.github.com>
2021-03-10 21:19:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -56,8 +56,6 @@ const checkPlaying = song =>{
     let elapsed = fakeDuration - currentTime;
     let seconds = Math.floor(elapsed % 60);
     let minutes = Math.floor(elapsed / 60);
-  }
-  
 // Animate the circle to denote time elapsed
     let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
     outline.style.strokeDashoffset = progress;
